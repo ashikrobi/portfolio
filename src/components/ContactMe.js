76830861715e0ffd2d3b7b900a74e7b39699eb0f
Ashiqur Rahman <ashikrobi@gmail.com>
2021-05-02 16:38:00 +0600

    Fixed some styles for contact form

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -1,87 +1,76 @@
 import React from "react";
 import {useForm, ValidationError} from "@formspree/react";
+const inputStyle = {
+  backgroundColor: "#1E242C",
+  color: "#757575",
+  border: "none",
+};
 
 const ContactMe = () => {
   let [state, handleSubmit] = useForm("xdoyazyw");
   if (state.succeeded) {
     return (
-      <form onSubmit={handleSubmit} className="col-md-3 col-xs-2 m-auto">
-        <div className="text-center mt-5 mb-3">
-          <h1>CONTACT</h1>
-          <p>Have a question or want to work together?</p>
-        </div>
-        <div className="mb-3">
-          <label htmlFor="firstName" className="form-label">
-            First Name
-          </label>
-          <input
-            id="firstName"
-            name="firstName"
-            type="text"
-            className="form-control"
-            placeholder="First Name"
-          />
-          <ValidationError
-            prefix="FirstName"
-            field="firstName"
-            errors={state.errors}
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="lastName" className="form-label">
-            Last Name
-          </label>
-          <input
-            id="lastName"
-            name="lastName"
-            type="text"
-            className="form-control"
-            placeholder="Last Name"
-          />
-          <ValidationError
-            prefix="LastName"
-            field="lastName"
-            errors={state.errors}
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="email" className="form-label">
-            Email address
-          </label>
-          <input
-            id="email"
-            type="email"
-            name="email"
-            className="form-control"
-            placeholder="Enter Email"
-          />
-          <ValidationError prefix="Email" field="email" errors={state.errors} />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="textArea" className="form-label">
-            Message
-          </label>
-          <textarea
-            id="message"
-            name="message"
-            className="form-control"
-            rows="5"></textarea>
-          <ValidationError
-            prefix="Message"
-            field="message"
-            errors={state.errors}
-          />
-        </div>
-        <div className="mb-3">
-          <button
-            type="submit"
-            disabled={state.submitting}
-            className="btn btn-primary">
-            Submit
-          </button>
-        </div>
-        <p className="text-success">Thanks for your inquiry.</p>
-      </form>
+      <div
+        style={{backgroundColor: "#252934", minHeight: "700px"}}
+        className="d-flex align-items-center">
+        <form onSubmit={handleSubmit} className="col-md-3 col-xs-2 m-auto">
+          <div className="text-center">
+            <h1 className="text-white mb-3">CONTACT</h1>
+            <p className="text-info mb-4">
+              Have a question or want to work together?
+            </p>
+          </div>
+          <div className="mb-3">
+            <input
+              id="name"
+              name="name"
+              type="text"
+              className="form-control"
+              placeholder="Name"
+              style={inputStyle}
+            />
+            <ValidationError prefix="Name" field="name" errors={state.errors} />
+          </div>
+          <div className="mb-3">
+            <input
+              id="email"
+              type="email"
+              name="email"
+              className="form-control"
+              placeholder="Enter Email"
+              style={inputStyle}
+            />
+            <ValidationError
+              prefix="Email"
+              field="email"
+              errors={state.errors}
+            />
+          </div>
+          <div className="mb-3">
+            <textarea
+              id="message"
+              name="message"
+              className="form-control mb-4"
+              rows="5"
+              placeholder="Your Message"
+              style={inputStyle}></textarea>
+            <ValidationError
+              prefix="Message"
+              field="message"
+              errors={state.errors}
+            />
+          </div>
+          <div className="mb-3">
+            <button
+              type="submit"
+              disabled={state.submitting}
+              className="btn btn-outline-danger">
+              Submit
+            </button>
+          </div>
+          <p className="text-success">Thanks for your inquiry.</p>;
+        </form>
+      </div>
     );
   }
   /* 
@@ -92,66 +81,42 @@ Main return statement down here. upper return is for only it the form successful
       style={{backgroundColor: "#252934", minHeight: "700px"}}
       className="d-flex align-items-center">
       <form onSubmit={handleSubmit} className="col-md-3 col-xs-2 m-auto">
-        <div className="text-center mt-5 mb-3">
-          <h1 className="text-white">CONTACT</h1>
-          <p className="text-info">Have a question or want to work together?</p>
+        <div className="text-center">
+          <h1 className="text-white mb-3">CONTACT</h1>
+          <p className="text-info mb-4">
+            Have a question or want to work together?
+          </p>
         </div>
         <div className="mb-3">
-          <label htmlFor="firstName" className="form-label">
-            First Name
-          </label>
           <input
-            id="firstName"
-            name="firstName"
+            id="name"
+            name="name"
             type="text"
             className="form-control"
-            placeholder="First Name"
+            placeholder="Name"
+            style={inputStyle}
           />
-          <ValidationError
-            prefix="FirstName"
-            field="firstName"
-            errors={state.errors}
-          />
+          <ValidationError prefix="Name" field="name" errors={state.errors} />
         </div>
         <div className="mb-3">
-          <label htmlFor="lastName" className="form-label">
-            Last Name
-          </label>
-          <input
-            id="lastName"
-            name="lastName"
-            type="text"
-            className="form-control"
-            placeholder="Last Name"
-          />
-          <ValidationError
-            prefix="LastName"
-            field="lastName"
-            errors={state.errors}
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="email" className="form-label">
-            Email address
-          </label>
           <input
             id="email"
             type="email"
             name="email"
             className="form-control"
             placeholder="Enter Email"
+            style={inputStyle}
           />
           <ValidationError prefix="Email" field="email" errors={state.errors} />
         </div>
         <div className="mb-3">
-          <label htmlFor="textArea" className="form-label">
-            Message
-          </label>
           <textarea
             id="message"
             name="message"
-            className="form-control"
-            rows="5"></textarea>
+            className="form-control mb-4"
+            rows="5"
+            placeholder="Your Message"
+            style={inputStyle}></textarea>
           <ValidationError
             prefix="Message"
             field="message"
--- src/components/Header.js
+++ src/components/Header.js
@@ -13,7 +13,12 @@ const Header = () => {
             Hello, I'm <span style={{color: "#E31B6D"}}>Ashiqur Rahman</span>.
           </h1>
           <h1 className="text-white mb-3">I'm a Frontend Web Developer.</h1>
-          <button className="btn btn-outline-danger">View My work</button>
+          <a
+            href="https://drive.google.com/uc?export=download&id=1YN6lpqHySlDfGeeDjGOAT9iwLDvN9SlF"
+            download
+            className="btn btn-outline-danger">
+            Download Resume
+          </a>
         </div>
       </div>
     </div>
