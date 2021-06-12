ac5650fc95e6e98053ba0a2fea9b7482e7225446
unknown <ashikrobi@gmail.com>
2021-06-12 16:50:45 +0600

    fixed responsiveness for footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
-import { faGithubSquare } from "@fortawesome/free-brands-svg-icons";
-import { faFacebookSquare } from "@fortawesome/free-brands-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faLinkedin} from "@fortawesome/free-brands-svg-icons";
+import {faGithubSquare} from "@fortawesome/free-brands-svg-icons";
+import {faFacebookSquare} from "@fortawesome/free-brands-svg-icons";
 import "../components/Footer.css";
 import ScrollToTop from "react-scroll-to-top";
 
@@ -19,37 +19,40 @@ const Footer = () => {
     return new Date().getFullYear();
   };
   return (
-    <div style={footerStyle} className='d-sm-flex justify-content-center'>
+    <div style={footerStyle} className="d-flex justify-content-center">
       <ScrollToTop
-        className='svg'
-        svgPath='M0,242.333h242.333V0H0V242.333z M49.749,148.309l-28.284-28.285l99.702-99.702l99.702,99.702l-28.284,28.285
-		l-53.244-53.245v124.663h-36.349V95.064L49.749,148.309z'
-        color='white'
-        viewBox='0 0 256 256'
+        className="svg"
+        svgPath="M0,242.333h242.333V0H0V242.333z M49.749,148.309l-28.284-28.285l99.702-99.702l99.702,99.702l-28.284,28.285
+		l-53.244-53.245v124.663h-36.349V95.064L49.749,148.309z"
+        color="white"
+        viewBox="0 0 256 256"
       />
-      <div className='d-sm-flex align-items-center'>
+      <div className="d-flex align-items-center">
         <div>
           <a
             href={"https://www.linkedin.com/in/ashikrobi/"}
-            className='text-reset mx-2'
-            style={fontIconSize}>
+            className="text-reset mx-2"
+            style={fontIconSize}
+          >
             <FontAwesomeIcon icon={faLinkedin} />
           </a>
           <a
             href={"https://github.com/ashikrobi"}
-            className='text-reset mx-2'
-            style={fontIconSize}>
+            className="text-reset mx-2"
+            style={fontIconSize}
+          >
             <FontAwesomeIcon icon={faGithubSquare} />
           </a>
           <a
             href={"https://www.facebook.com/ashikrobi"}
-            className='text-reset mx-2'
-            style={fontIconSize}>
+            className="text-reset mx-2"
+            style={fontIconSize}
+          >
             <FontAwesomeIcon icon={faFacebookSquare} />
           </a>
-          <small style={{ display: "block" }}>
+          <small style={{display: "block"}}>
             ASHIQUR RAHMAN
-            <span style={{ color: "#E31B6D" }}>&copy;{showCurrentYear()}</span>
+            <span style={{color: "#E31B6D"}}>&copy;{showCurrentYear()}</span>
           </small>
         </div>
       </div>
